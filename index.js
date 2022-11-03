deac845094ce830cead91d6ca5217f446225d78d
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-11-03 10:16:50 -0400

    .

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-const button = document.getElementById("btn");
-console.log(button);
+// const DOMSelectors = {
+//   button: document.getElementById("btn"),
+//   text: document.querySelector("#text"),
+//   points: document.querySelectorAll(".point"),
+//   box: document.getElementById("big-black-box"),
+// };
+
+// function backgroundAndText(background, text) {
+//   background.style.backgroundColor = "blue";
+//   text.textContent = "This is now a bigger blue box";
+//   text.style.fontSize = "40px";
+// }
+
+// DOMSelectors.button.addEventListener("click", function () {
+//   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+// });
+
+// function changerLi() {
+//   let pointIndex = 1;
+//   DOMSelectors.points.forEach((point) => {
+//     point.addEventListener("click", function () {
+//       point.textContent = `Hello I am point ${pointIndex}`;
+//       pointIndex--;
+//     });
+//   });
+// }
+// changerLi();
+
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`#input`),
+};
+
+// const cat = "meow";
+// DOMSelectors.box.insertAdjacentHTML("afterend", `<h1>We are an </h1>`);
+DOMSelectors.button.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("afterend", <p>${input}</p>);
+});
