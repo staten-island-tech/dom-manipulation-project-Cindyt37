0db2c92bbcde6b8b38eaf69bc2d535b0e7f44d0e
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-11-07 09:41:53 -0500

    .

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-// const DOMSelectors = {
-//   button: document.getElementById("btn"),
-//   text: document.querySelector("#text"),
-//   points: document.querySelectorAll(".point"),
-//   box: document.getElementById("big-black-box"),
-// };
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  points: document.querySelectorAll(".point"),
+  box: document.getElementById("big-black-box"),
+};
+DOMSelectors.button.addEventListener("click", function () {
+  backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+});
 
-// function backgroundAndText(background, text) {
-//   background.style.backgroundColor = "blue";
-//   text.textContent = "This is now a bigger blue box";
-//   text.style.fontSize = "40px";
-// }
-
-// DOMSelectors.button.addEventListener("click", function () {
-//   backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-// });
-
-// function changerLi() {
-//   let pointIndex = 1;
-//   DOMSelectors.points.forEach((point) => {
-//     point.addEventListener("click", function () {
-//       point.textContent = `Hello I am point ${pointIndex}`;
-//       pointIndex--;
-//     });
-//   });
-// }
-// changerLi();
+function changerLi() {
+  let pointIndex = 1;
+  DOMSelectors.points.forEach((point) => {
+    point.addEventListener("click", function () {
+      point.textContent = `Hello I am point ${pointIndex}`;
+      pointIndex--;
+    });
+  });
+}
+changerLi();
 
 const DOMSelectors = {
   button: document.getElementById("btn"),
@@ -37,5 +30,6 @@ const DOMSelectors = {
 DOMSelectors.button.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${input}</p>`);
+  DOMSelectors.input.value = "";
   console.log(input);
 });
