5ceee9c22e6eeaf7657e13164a4221acf4bfea22
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-11-09 10:16:54 -0500

    .

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const DOMSelectors = {
-  button: document.getElementById("Enter"),
-  name: document.querySelector(`name`),
-  description: document.querySelector(`description`),
-  url: document.querySelector(`url`),
-  container: document.getElementById("container"),
+  button: document.getElementById("btn"),
+  box: document.getElementById("container-box"),
+  input: document.querySelector(`#name`),
+  input2: document.querySelector(`#weapon`),
+  input3: document.querySelector(`#url`),
+  output: document.getElementById("output"),
 };
 
 DOMSelectors.button.addEventListener("click", function () {
-  let name = DOMSelectors.name.value;
-  DOMSelectors.box.insertAdjacentHTML("afterend", `<p>${name}</p>`);
+  let input = DOMSelectors.input.value;
   DOMSelectors.input.value = "";
+  let input2 = DOMSelectors.input2.value;
+  DOMSelectors.input2.value = "";
+  let input3 = DOMSelectors.input3.value;
+  DOMSelectors.input3.value = "";
+  DOMSelectors.output.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="output">
+        <h3>${input}</h3>
+       <h3>${input2}</h3>
+        <img src="${input3}">
+        <br>
+        <button class= "button" onclick="this.parentElement.Remove()">Remove</button></div>`
+  );
 });
