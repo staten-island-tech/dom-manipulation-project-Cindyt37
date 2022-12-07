5a43805327fc3ff39cf9c57df56f863e9a96ffd0
Cindyt37 <113190202+Cindyt37@users.noreply.github.com>
2022-12-07 09:47:59 -0500

    .

--- index.js
+++ index.js
@@ -26,65 +26,19 @@ function enter() {
         <button class="remove">Remove</button>
         </div>`
   );
-
-  function removeCard() {
-    const removes = document.querySelectorAll(".remove");
-    removes.forEach((eachRemove) => {
-      eachRemove.addEventListener("click", function (button) {
-        button.target.parentElement.remove();
-      });
-    });
-  }
-
-  function clear() {
-    DOMSelectors.input.value = "";
-    DOMSelectors.input2.value = "";
-    DOMSelectors.input3.value = "";
-  }
 }
 
-DOMSelectors = {
-  form: document.getElementById("form"),
-  description: document.getElementById("description"),
-  name: document.getElementById("name"),
-  image: document.getElementById("image"),
-  output: document.getElementById("output"),
-};
-
-DOMSelectors.form.addEventListener("submit", function () {
-  event.preventDefault();
-  enterFunction();
-  removebutton();
-  resetInputs();
-});
-
-function enterFunction() {
-  descript = DOMSelectors.description.value;
-  title = DOMSelectors.name.value;
-  image = DOMSelectors.image.value;
-  DOMSelectors.output.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="output">
-                <h2>${title}</h2>
-                <p>${descript}</p>
-                <img class="output-image" src="${image}">
-                <br>
-                <button class="button">Remove</button>
-        </div>`
-  );
-}
-
-function removebutton() {
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      this.parentElement.remove();
+function removeCard() {
+  const removes = document.querySelectorAll(".remove");
+  removes.forEach((eachRemove) => {
+    eachRemove.addEventListener("click", function (button) {
+      button.target.parentElement.remove();
     });
   });
 }
 
-function resetInputs() {
-  DOMSelectors.description.value = "";
-  DOMSelectors.name.value = "";
-  DOMSelectors.image.value = "";
+function clear() {
+  DOMSelectors.input.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
 }
